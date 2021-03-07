10752638d909cb211b2f745be867bef30987f05a
mcqueeen <mcqueeen13@gmail.com>
2021-03-07 14:35:47 +0200

    homework

---
+++ home work 1/homewok.py
@@ -0,0 +1,49 @@
+#перша задача
+bdgola=2
+bdgola1=2*10
+sum=bdgola1
+print(bdgola1)
+
+#друга задача 
+Mincrafter1999=2000
+DoctorHto=50
+dif=Mincrafter1999-DoctorHto
+print(dif)
+
+#третя задача
+DoctorHto=5
+pereglyadiv=1000
+sum=DoctorHto*pereglyadiv
+print(sum)
+
+#четверта задача
+katya=10
+katya1=15
+kartyn=5
+sum=(katya1-katya)*kartyn
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
